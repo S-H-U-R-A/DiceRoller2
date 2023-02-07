1862d8dccee5ff8a597febd86a3af7f459361ee3
sandres0190 <sandres0190@gmail.com>
2023-02-07 11:42:08 -0500

    01.3: recursos de imagen y compatibilidad

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,41 +2,52 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var diceImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        diceImage = findViewById(R.id.dice_image)
+
         val rollButton: Button = findViewById(R.id.roll_button)
 
-        rollButton.setOnClickListener(){
+        rollButton.setOnClickListener{
             rollDice()
         }
+
+        /*HomeWork
 
         val countUp: Button = findViewById(R.id.count_up_button)
 
         countUp.setOnClickListener{
             countUp()
-        }
+        }*/
     }
 
     private fun rollDice(){
 
-        val randomInt: Int = (1..6).random()
+        val drawableResource: Int = when( (1..6).random() ){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
-        val resultText: TextView = findViewById(R.id.result_text)
+        diceImage.setImageResource(drawableResource)
 
-        resultText.text = randomInt.toString()
-
-        Toast.makeText( this, "Button clicked", Toast.LENGTH_SHORT).show()
+        //Toast.makeText( this, "Button clicked", Toast.LENGTH_SHORT).show()
     }
 
-    private fun countUp(){
+
+    /*private fun countUp(){
 
         val resultText: TextView = findViewById(R.id.result_text)
 
@@ -53,5 +64,5 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-    }
+    }*/
 }
